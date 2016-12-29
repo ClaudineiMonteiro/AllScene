d1e6075145bfb0ec89bd7d622b0499e4bbb75991
Claudinei Monteiro <claudinei.monteiro@outlook.com>
2016-12-29 16:01:21 -0200

    [add] Migrations

---
+++ src/AllScene.Infra.Data/Context/AllSceneContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using AllScene.Domain.Entities;
+using AllScene.Infra.Data.EntityConfig;
+
+namespace AllScene.Infra.Data.Context
+{
+	public class AllSceneContext : DbContext
+	{
+		#region Constructors
+		public AllSceneContext():base("AllScene"){}
+		#endregion
+
+		#region Properties
+		public DbSet<Seguiment> Seguiments { get; set; }
+		public DbSet<Member> Members { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+		public DbSet<Artist> Artists { get; set; }
+		#endregion
+
+		#region Methods
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+			modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+			modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+			// General Custom Context Properties
+			modelBuilder.Properties()
+				.Where(p => p.Name == p.ReflectedType.Name + "Id")
+				.Configure(p => p.IsKey());
+
+			modelBuilder.Properties<string>()
+				.Configure(p => p.HasColumnType("varchar"));
+
+			modelBuilder.Properties<string>()
+				.Configure(p => p.HasMaxLength(100));
+
+			modelBuilder.Configurations.Add(new SeguimentConfig());
+			base.OnModelCreating(modelBuilder);
+		}
+
+		#endregion
+	}
+}
---
+++ src/AllScene.Infra.Data/EntityConfig/SeguimentConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using AllScene.Domain.Entities;
+
+namespace AllScene.Infra.Data.EntityConfig
+{
+	class SeguimentConfig : EntityTypeConfiguration<Seguiment>
+	{
+		#region Constructors
+		public SeguimentConfig()
+		{
+			HasKey(c => c.ArtistId);
+
+			Property(c => c.Description)
+				.IsRequired()
+				.HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute() { IsUnique = true }));
+
+			Ignore(c => c.ValidationResult);
+		}
+		#endregion
+	}
+}
