fbcb88ddff657639ec8d51dd6ef97eee28733c26
claud <claud@DESKTOP-R0SREQ5>
2017-01-01 20:19:25 -0200

    [add] Pattern interface repository

---
+++ src/AllScene.Domain/Interfaces/Repository/ISeguimentRepositorycs.cs
@@ -0,0 +1,9 @@
+﻿using AllScene.Domain.Entities;
+
+namespace AllScene.Domain.Interfaces.Repository
+{
+	public interface ISeguimentRepositorycs : IRepository<Seguiment>
+	{
+		Seguiment GetByDescription(string description);
+	}
+}
