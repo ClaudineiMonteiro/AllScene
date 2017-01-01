cef9d04cbbcc8ead27d980596adaa37f1999c803
claud <claud@DESKTOP-R0SREQ5>
2017-01-01 19:28:35 -0200

    [change] Impementetion of unitofwork

--- src/AllScene.Infra.Data/UoW/UnitOfWork.cs
+++ src/AllScene.Infra.Data/UoW/UnitOfWork.cs
@@ -1,6 +1,53 @@
-﻿namespace AllScene.Infra.Data.UoW
+﻿using System;
+using AllScene.Infra.Data.Context;
+using AllScene.Infra.Data.Interfaces;
+
+namespace AllScene.Infra.Data.UoW
 {
-	public class UnitOfWork
+	public class UnitOfWork :IUnitOfWork
 	{
+		#region Attributes
+
+		private readonly AllSceneContext _context;
+		private bool _disposed;
+		#endregion
+
+		#region Constructors
+
+		public UnitOfWork(AllSceneContext context)
+		{
+			_context = context;
+		}
+		#endregion
+
+		#region Methods
+		public void BeginTransection()
+		{
+			_disposed = false;
+		}
+
+		public void Commit()
+		{
+			_context.SaveChanges();
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposed)
+			{
+				if (disposing)
+				{
+					_context.Dispose();
+				}
+			}
+			_disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		#endregion
 	}
 }
