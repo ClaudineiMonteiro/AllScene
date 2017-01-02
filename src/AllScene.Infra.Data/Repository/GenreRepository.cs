c7f8ddd52633a84f4549e8ab0f01e3a10b855455
Claudinei Monteiro <claudinei.monteiro@outlook.com>
2017-01-02 13:45:45 -0200

    [add] Genre class data repository.

---
+++ src/AllScene.Infra.Data/Repository/GenreRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.Linq;
+using AllScene.Domain.Entities;
+using AllScene.Domain.Interfaces.Repository;
+using AllScene.Infra.Data.Context;
+using Dapper;
+
+namespace AllScene.Infra.Data.Repository
+{
+	public class GenreRepository : Repository<Genre>, IGenreRepository
+	{
+		private DbConnection cn;
+		public GenreRepository(AllSceneContext context) : base(context)
+		{
+			cn = Db.Database.Connection;
+		}
+
+		public Genre GetByDescription(string description)
+		{
+			return Search(c => c.Description.Equals(description)).FirstOrDefault();
+		}
+
+		public override IEnumerable<Genre> GetAll()
+		{
+			var sql = @"
+SELECT *
+  FROM Genre
+";
+			return cn.Query<Genre>(sql);
+		}
+
+		public override Genre GetById(Guid id)
+		{
+			var sql = @"
+SELECT A.*
+  FROM Genre A
+ WHERE A.GenreId = @GenreId
+";
+			var genre = cn.Query<Genre>(sql, new {GenreId = id}).FirstOrDefault();
+			return genre;
+		}
+	}
+}
