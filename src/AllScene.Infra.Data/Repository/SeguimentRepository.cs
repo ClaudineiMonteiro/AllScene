6826c63e24776052bd07b14973c22eb185bcb738
claud <claud@DESKTOP-R0SREQ5>
2017-01-01 23:22:08 -0200

    [add] Repository of seguiment

--- src/AllScene.Infra.Data/Repository/SeguimentRepository.cs
+++ src/AllScene.Infra.Data/Repository/SeguimentRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace AllScene.Infra.Data.Repository
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AllScene.Domain.Entities;
+using AllScene.Domain.Interfaces.Repository;
+using AllScene.Infra.Data.Context;
+using Dapper;
+
+namespace AllScene.Infra.Data.Repository
 {
-	public class SeguimentRepository
+	public class SeguimentRepository : Repository<Seguiment>, ISeguimentRepositorycs
 	{
+		public SeguimentRepository(AllSceneContext context) : base(context)
+		{
+		}
+
+		public Seguiment GetByDescription(string description)
+		{
+			return Search(c => c.Description.Equals(description)).FirstOrDefault();
+		}
+
+		public override IEnumerable<Seguiment> GetAll()
+		{
+			var cn = Db.Database.Connection;
+			var sql = @"
+SELECT *
+  FROM Sequiment
+";
+			return cn.Query<Seguiment>(sql);
+		}
+
+		public override Seguiment GetById(Guid id)
+		{
+			var cn = Db.Database.Connection;
+			var sql = @"
+SELECT A.*
+  FROM Seguiment A
+ WHERE A.SeguimentId = @SeguimentId
+";
+			var seguiment = cn.Query<Seguiment>(sql, new {SeguimentId = id});
+			return seguiment.FirstOrDefault();
+		}
 	}
 }
