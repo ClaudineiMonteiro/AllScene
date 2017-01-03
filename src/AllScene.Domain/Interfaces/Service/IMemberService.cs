3a24c75e79290488624e363fbd89e5ecdd79cfad
Claudinei Monteiro <claudinei.monteiro@outlook.com>
2017-01-03 14:32:33 -0200

    [add] Member services.

---
+++ src/AllScene.Domain/Interfaces/Service/IMemberService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using AllScene.Domain.Entities;
+
+namespace AllScene.Domain.Interfaces.Service
+{
+	public interface IMemberService : IDisposable
+	{
+		Member Add(Member genre);
+		Member GetById(Guid id);
+		Member GetByDescription(string description);
+		IEnumerable<Member> GetAll();
+		Member Update(Member genre);
+		void Remove(Guid id);
+	}
+}
---
+++ src/AllScene.Domain/Services/MemberService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using AllScene.Domain.Entities;
+using AllScene.Domain.Interfaces.Repository;
+using AllScene.Domain.Interfaces.Service;
+
+namespace AllScene.Domain.Services
+{
+	public class MemberService : IMemberService
+	{
+		#region Attrubes
+		private readonly IMemberRepository _memberRepository;
+		#endregion
+
+		#region
+		public MemberService(IMemberRepository memberRepository)
+		{
+			_memberRepository = memberRepository;
+		}
+		#endregion
+
+		#region Methods
+		public void Dispose()
+		{
+			_memberRepository.Dispose();
+			GC.SuppressFinalize(this);
+		}
+
+		public Member Add(Member genre)
+		{
+			return _memberRepository.Add(genre);
+		}
+
+		public Member GetById(Guid id)
+		{
+			return _memberRepository.GetById(id);
+		}
+
+		public Member GetByDescription(string description)
+		{
+			return GetByDescription(description);
+		}
+
+		public IEnumerable<Member> GetAll()
+		{
+			return _memberRepository.GetAll();
+		}
+
+		public Member Update(Member genre)
+		{
+			return _memberRepository.Update(genre);
+		}
+
+		public void Remove(Guid id)
+		{
+			_memberRepository.Remove(id);
+		}
+		#endregion
+	}
+}
