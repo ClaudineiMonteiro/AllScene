575414e17d9de0daea3dad49e62c22e94b0a890c
Claudinei Monteiro <claudinei.monteiro@outlook.com>
2017-01-05 17:57:45 -0200

    [add] Genre validations and specifications.

---
+++ src/AllScene.Domain/Validations/Genres/GenreIsConsistentValidation.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using AllScene.Domain.Entities;
+using DomainValidation.Validation;
+
+namespace AllScene.Domain.Validations.Genres
+{
+	public class GenreIsConsistentValidation : Validator<Genre>
+	{
+		#region Methods
+
+		public bool IsSatisfiedBy(Genre genre)
+		{
+			return genre.Description.Length > 0;
+		}
+		#endregion
+	}
+}
